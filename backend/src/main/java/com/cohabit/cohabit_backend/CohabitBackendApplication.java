762d03b9666c8e6b042d243bad15fd506b32a08e
danielmi5 <midaniel@iesrafaelalberti.es>
2025-12-07 19:56:58 +0100

    Creada estructura inicial del backend con Spring Boot.

---
+++ backend/src/main/java/com/cohabit/cohabit_backend/CohabitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cohabit.cohabit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CohabitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CohabitBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cohabit/cohabit_backend/CohabitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cohabit.cohabit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CohabitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

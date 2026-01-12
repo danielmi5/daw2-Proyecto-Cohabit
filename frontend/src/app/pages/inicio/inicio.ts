17e12eef6d8e6a3173f4dc395a7282b9d70fbfcc
danielmi5 <midaniel@iesrafaelalberti.es>
2026-01-12 18:17:42 +0100

    Se han creado las secciones faltantes de la página de inicio con su estructura HTML y diseño. Error arreglado en el footer.

--- frontend/src/app/pages/inicio/inicio.ts
+++ frontend/src/app/pages/inicio/inicio.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Accordion } from '../../components/shared/accordion/accordion';
 import { AccordionItem } from '../../components/shared/accordion/accordion-item';
+import { FeatherIconDirective } from '../../directives/feather-icon.directive';
 
 @Component({
   selector: 'app-inicio',
-  imports: [RouterLink, Accordion, AccordionItem],
+  imports: [RouterLink, Accordion, AccordionItem, FeatherIconDirective],
   templateUrl: './inicio.html',
   styleUrl: './inicio.scss',
 })

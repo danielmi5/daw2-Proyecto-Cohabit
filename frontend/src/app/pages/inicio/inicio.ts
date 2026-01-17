95aaae7729eb43d2fabea52742db787ea9312951
danielmi5 <midaniel@iesrafaelalberti.es>
2026-01-17 16:55:07 +0100

    Añadido estado para la sección inicial de la página de inicio, para que muestre los botones 'Iniciar sesión' / 'Comenzar ahora' si está autenticado y si no, botón 'Ir a Dashboard'. Además, se arregla el scss del footer.

--- frontend/src/app/pages/inicio/inicio.ts
+++ frontend/src/app/pages/inicio/inicio.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Accordion } from '../../components/shared/accordion/accordion';
 import { AccordionItem } from '../../components/shared/accordion/accordion-item';
 import { Paso } from '../../components/shared/paso/paso';
 import { Caracteristica } from '../../components/shared/caracteristica/caracteristica';
-import { FeatherIconDirective } from '../../directives/feather-icon.directive';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-inicio',
-  imports: [RouterLink, Accordion, AccordionItem, FeatherIconDirective, Paso, Caracteristica],
+  imports: [RouterLink, Accordion, AccordionItem, Paso, Caracteristica],
   templateUrl: './inicio.html',
   styleUrl: './inicio.scss',
 })
 export class Inicio {
-
+  authService = inject(AuthService);
 }

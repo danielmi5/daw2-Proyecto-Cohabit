fef2409ff14a6e40411e9d79eb9051a47d2d6eab
danielmi5 <midaniel@iesrafaelalberti.es>
2026-02-07 17:21:43 +0100

    Página de perfil creada, con ruta actualizada para datos personales.

--- frontend/src/app/pages/perfil/perfil.ts
+++ frontend/src/app/pages/perfil/perfil.ts
@@ -1,26 +1,15 @@
-import { Component, inject } from '@angular/core';
-import { Button } from '../../components/shared/button/button';
-import { NotificacionService } from '../../services/notificacion.service';
-import { AuthService } from '../../services/auth.service';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Sidebar } from '../../components/layout/sidebar/sidebar';
 
-// Página principal del perfil (entrada a sección perfil con navegación a subpáginas)
+// Contenedor del perfil con sidebar y RouterOutlet (protegido con authGuard)
 @Component({
   selector: 'app-perfil',
-  imports: [Button],
+  imports: [RouterOutlet, Sidebar],
   templateUrl: './perfil.html',
   styleUrls: ['./perfil.scss'],
 })
 export class Perfil {
-  private notificationService = inject(NotificacionService);
-  private authService = inject(AuthService);
-  
-  cerrarSesion(): void{
-    try {
-      this.authService.cerrarSesion()
-      this.notificationService.success("Se ha cerrado la sesión correctamente");
-    } catch (error) {
-      this.notificationService.error("Ha ocurrido un error. No se ha podido cerrar sesión.");
-    }
-  }
+
 }
 

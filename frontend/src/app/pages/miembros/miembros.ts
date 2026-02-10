785a982a649bada2f852e9aefd4b6e4d883666cf
danielmi5 <midaniel@iesrafaelalberti.es>
2026-02-10 10:52:46 +0100

    Lógica añadida a miembros.ts para cargar los datos.

--- frontend/src/app/pages/miembros/miembros.ts
+++ frontend/src/app/pages/miembros/miembros.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { UsuarioService } from '../../services/usuario.service';
+import { NotificacionService } from '../../services/notificacion.service';
+import { Spinner } from '../../components/shared/spinner/spinner';
+import { CommonModule } from '@angular/common';
+import { MiembroGrupoResponse, UsuarioResponse } from '../../models';
+import { MiembroGrupoService } from '../../services/miembro-grupo.service';
+import { Router } from '@angular/router';
 
-// Gestión de miembros del grupo (listar, invitar, gestionar roles)
 @Component({
   selector: 'app-miembros',
-  imports: [],
+  imports: [CommonModule, Spinner],
   templateUrl: './miembros.html',
-  styleUrls: ['./miembros.scss'],
+  styleUrl: './miembros.scss',
 })
-export class Miembros {
+export class Miembros implements OnInit{
+  private authService = inject(AuthService);
+  private usuarioService = inject(UsuarioService);
+  private miembroService = inject(MiembroGrupoService);
+  private notificacionService = inject(NotificacionService);
+  private router = inject(Router);
 
+  usuarioActual = signal<UsuarioResponse | null>(null);
+  miembroActual = signal<MiembroGrupoResponse | null>(null);
+  total = 0;
+  error = false;
+  mensajeError = "";
+  cargando = signal<boolean>(true);
+
+  grupoId: number | null = null;
+  miembroId: number | null = null;
+  
+  ngOnInit(): void {
+    this.cargarMiembros();
+  }
+
+  private cargarMiembros(){
+    const usuario = this.authService.usuarioDetalles();
+    
+    if (!usuario?.miembroGrupoId) {
+      this.error = true;
+      this.mensajeError = 'No perteneces a ningún grupo';
+      this.cargando.set(false);
+      return;
+    } else{
+      this.usuarioActual.set(usuario);
+    }
+
+    this.miembroId = usuario.miembroGrupoId;
+
+    this.miembroService.get(usuario.miembroGrupoId).subscribe({
+      next: (miembro) => {
+        this.miembroActual.set(miembro);
+        if (this.grupoId) {
+          this.cargarMiembros();
+        } else {
+          this.error = true;
+          this.mensajeError = 'No se pudo obtener el ID del grupo';
+          this.cargando.set(false);
+        }
+      },
+      error: (error) => {
+        console.error('Error al cargar datos del miembro:', error);
+        this.error = true;
+        this.mensajeError = 'Error al cargar los datos del grupo';
+        this.cargando.set(false);
+      }
+    });
+  }
 }
